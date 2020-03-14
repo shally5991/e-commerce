9d10744c0e7ffb1b766642b14dfa41da95d033c2
shally5991 <shailja5991@gmail.com>
2020-03-14 13:10:00 +0530

    Updates

---
+++ precache-manifest.50401db7cfa94b39810b69b89f91e0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97128e16c43148c2f7115346bd3016a1",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "d5514900bbd98879af97",
+    "url": "/e-commerce/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6193b87a45ecb48c94b9",
+    "url": "/e-commerce/static/js/2.f568408d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/e-commerce/static/js/2.f568408d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5514900bbd98879af97",
+    "url": "/e-commerce/static/js/main.d2546acf.chunk.js"
+  },
+  {
+    "revision": "000527da91832500655a",
+    "url": "/e-commerce/static/js/runtime-main.aa9b728f.js"
+  }
+]);
